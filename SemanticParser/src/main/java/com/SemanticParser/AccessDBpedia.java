5a4dbfef0ecce64021baf0122b9fa57365db1d08
angelacoconuts <angelacoconuts@gmail.com>
2014-01-03 08:22:23 +0800

    Revised DBpedia access

--- SemanticParser/src/main/java/com/SemanticParser/AccessDBpedia.java
+++ SemanticParser/src/main/java/com/SemanticParser/AccessDBpedia.java
@@ -1,55 +1,138 @@
 package com.SemanticParser;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.hp.hpl.jena.query.ParameterizedSparqlString;
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolutionMap;
 import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFormatter;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.sparql.engine.http.QueryExceptionHTTP;
 
 public class AccessDBpedia {
-	
+
 	private String testEndpointQuery = "ASK { }";
-	
+
 	/**
 	 * Fire a SPARQL against service endpoint
-	 * @param serviceEndpoint SPARQL service endpoint e.g: http://dbpedia.org/sparql
-	 * @param sparqlQueryString  SPARQL query string
+	 * 
+	 * @param serviceEndpoint
+	 *            SPARQL service endpoint e.g: http://dbpedia.org/sparql
+	 * @param sparqlQueryString
+	 *            SPARQL query string
+	 * @param prefixList
+	 *            List of prefixes in the query
 	 * @return com.hp.hpl.jena.query.ResultSet
 	 */
-	public ResultSet execSelect(String serviceEndpoint, String sparqlQueryString){
-	
-    	Query query = QueryFactory.create(sparqlQueryString);
-    	
-    	QueryExecution qexec = QueryExecutionFactory.sparqlService(serviceEndpoint, query);
-        
-    	ResultSet results = qexec.execSelect();
-    	
-    	return results;
-    	
- 	}
-	
+	public ResultSet execSimpleSelect(String serviceEndpoint,
+			String sparqlQueryString, List<String> prefixList) {
+
+		Query query = createSimpleQuery(sparqlQueryString, prefixList);
+
+		ResultSet results = execSelectQuery(serviceEndpoint, query);
+
+		return results;
+
+	}
+
+	public Query createParameterizedQuery(String sparqlQueryString,
+			List<String> prefixList, Map<String, String> initialBindingSet) {
+
+		Model model = ModelFactory.createDefaultModel();
+
+		QuerySolutionMap initialBindings = new QuerySolutionMap();
+
+		for (Map.Entry<String, String> bindingEntry : initialBindingSet
+				.entrySet()) {
+
+			RDFNode bindingNode = model.createResource(bindingEntry.getValue());
+			initialBindings.add(bindingEntry.getKey(), bindingNode);
+
+		}
+
+		ParameterizedSparqlString queryStr = new ParameterizedSparqlString(
+				sparqlQueryString, initialBindings);
+
+		setPrefixes(queryStr, prefixList);
+
+		Query query = queryStr.asQuery();
+		return query;
+
+	}
+
+	public Query createSimpleQuery(String sparqlQueryString,
+			List<String> prefixList) {
+
+		ParameterizedSparqlString queryStr = new ParameterizedSparqlString(
+				sparqlQueryString);
+
+		setPrefixes(queryStr, prefixList);
+
+		Query query = queryStr.asQuery();
+
+		return query;
+	}
+
+	public ResultSet execSelectQuery(String serviceEndpoint, Query query) {
+
+		ResultSet results = null;
+		ByteArrayOutputStream logWriter = new ByteArrayOutputStream();
+
+		QueryExecution qexec = QueryExecutionFactory.sparqlService(
+				serviceEndpoint, query);
+
+		results = qexec.execSelect();
+		
+		ResultSetFormatter.out(logWriter, results, query);
+		App.logger.debug(logWriter.toString());
+
+		return results;
+
+	}
+
+	public ParameterizedSparqlString setPrefixes(
+			ParameterizedSparqlString queryStr, List<String> prefixList) {
+
+		for (String prefix : prefixList)
+			queryStr.setNsPrefix(prefix, App.prefixMap.get(prefix));
+
+		return queryStr;
+
+	}
+
 	/**
 	 * Test whether a service endpoint is up and running or not
+	 * 
 	 * @param serviceEndpoint
 	 * @return test result (boolean)
 	 */
-	public boolean testEndpoint(String serviceEndpoint){
-		
-    	Query query = QueryFactory.create(testEndpointQuery);
-    	
-    	QueryExecution qexec = QueryExecutionFactory.sparqlService(serviceEndpoint, query);
-        
-    	try{
-    		if(qexec.execAsk()){
-    			App.logger.info(serviceEndpoint + " is UP!");
-    			return true;
-    		}
-     	} catch (QueryExceptionHTTP e){
+	public boolean testEndpoint(String serviceEndpoint) {
+
+		Query query = QueryFactory.create(testEndpointQuery);
+
+		QueryExecution qexec = QueryExecutionFactory.sparqlService(
+				serviceEndpoint, query);
+
+		try {
+			if (qexec.execAsk()) {
+				App.logger.info(serviceEndpoint + " is UP!");
+				return true;
+			}
+		} catch (QueryExceptionHTTP e) {
 			App.logger.error(serviceEndpoint + " is DOWN!");
 			return false;
-     	}
+		}
 		return false;
-    	
- 	}
+
+	}
 }
--- SemanticParser/src/main/java/com/SemanticParser/App.java
+++ SemanticParser/src/main/java/com/SemanticParser/App.java
@@ -2,24 +2,35 @@ package com.SemanticParser;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
+//import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 
 import com.hp.hpl.jena.query.QuerySolution;
-//import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSet;
 
 public class App 
 {
 	protected static Logger logger = Logger.getLogger(App.class.getName());
 	
+    public static Map<String, String> prefixMap = new HashMap<String, String>();
+    static{
+            prefixMap.put("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+            prefixMap.put("rdfs", "http://www.w3.org/2000/01/rdf-schema#");
+            prefixMap.put("dbpedia-owl", "http://dbpedia.org/ontology/");
+    }
+	
     public static void main( String[] args )
     {
     	
 
-/*    	String sparqlQueryString = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
+    	String sparqlQueryString = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
     			+ "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
     			+ "SELECT * WHERE {"
     			+ "?subject rdf:type <http://dbpedia.org/class/yago/EuropeanCountries>."
@@ -27,12 +38,14 @@ public class App
     	String service = "http://dbpedia.org/sparql";
     	
     	AccessDBpedia query = new AccessDBpedia();
-    	ResultSet results = query.execSelect( service, sparqlQueryString );
+    	List<String> prefixList = new ArrayList<String>();
+    	prefixList.add("rdf");
+    	prefixList.add("rdfs");    	
+		ResultSet results = query.execSimpleSelect( service, sparqlQueryString, prefixList );
     	while(results.hasNext()){
     		QuerySolution result = results.next();
     		System.out.println(result.get("subject").toString());
     	}
-//    	ResultSetFormatter.out(System.out, results, query);*/
 
     }
 }
